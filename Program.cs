b610e9702a94ab2d4c7a691684c8a0f4429897b0
Andrew Washburn <alwashburn91@gmail.com>
2024-01-01 22:04:14 +0000

    initial commit

--- Program.cs
+++ Program.cs
@@ -1,34 +1,31 @@
+using DishesApi.DbContexts;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<DishesDbContext>(
+    o => o.UseSqlite(builder.Configuration["ConnectionStrings:DishesDBConnectionString"])
+);
+
 var app = builder.Build();
 
-app.Urls.Add("http://localhost:5000");
+app.Urls.Add("https://localhost:5000");
 
 app.MapGet("/", () => "Hello World!");
 
-app.MapGet("/{cityName}/weather", GetWeatherByCity);
+app.MapGet(
+    "dishes",
+    async (DishesDbContext db) =>
+    {
+        return await db.Dishes.ToListAsync();
+    }
+);
+
+using (var serviceScope = app.Services.GetService<IServiceScopeFactory>()!.CreateScope())
+{
+    var context = serviceScope.ServiceProvider.GetRequiredService<DishesDbContext>();
+    context.Database.EnsureDeleted();
+    context.Database.Migrate();
+}
 
 app.Run();
-
-
-Weather GetWeatherByCity(string cityName)
-{
-    app.Logger.LogInformation($"Weather requested for {cityName}.");
-    var weather = new Weather(cityName);
-    return weather;
-}
-
-public record Weather
-{
-    public string City { get; set; }
-
-    public Weather(string city)
-    {
-        City = city;
-        Conditions = "Cloudy";
-        // Temperature here is in celsius degrees, hence the 0-40 range.
-        Temperature = new Random().Next(0,40).ToString();
-    }
-
-    public string Conditions { get; set; }
-    public string Temperature { get; set; }
-}
